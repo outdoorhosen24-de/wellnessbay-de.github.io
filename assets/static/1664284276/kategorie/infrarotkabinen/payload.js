fa9ff084a1122daf54f03e9fcc8a7baf4e41ebc6
outdoorhosen24-de <outdoorhosen24-de@users.noreply.github.com>
2022-09-27 13:11:54 +0000

    deploy: d4b307b73839f28d621d1bea33c05227c386c3cf

---
+++ assets/static/1664284276/kategorie/infrarotkabinen/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/kategorie/infrarotkabinen", {data:[{_img:{"/_ipx/f_webp,q_90,s_407x229/assets/images/images/sanotechnik-infrarotkabine-calipso-fuer-3-personen-sanotechnik-6045348-1.jpg":"\u002Fassets\u002Fimages\u002Fsanotechnik-infrarotkabine-calipso-fuer-3-personen-sanotechnik-6045348-1.webp"}}],fetch:{},mutations:void 0});
